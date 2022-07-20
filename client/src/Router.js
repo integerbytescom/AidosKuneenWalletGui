4c0c996e024b9cb41ab1bacd9e93168438e79509
integ <cergocergo41@gmail.com>
2022-07-20 17:40:29 +0300

    client v1.8

--- client/src/Router.js
+++ client/src/Router.js
@@ -1,24 +1,15 @@
 import React from 'react';
 import {Route,Routes} from "react-router-dom";
-import PreviewPage from "./pages/preview-page/PreviewPage";
-import ChoosePage from "./pages/choose-page/ChoosePage";
-import LocalWalletStartPage from "./pages/local-wallet-start/LocalWalletStartPage";
-import TransfersPage from "./pages/transfers-page/TransfersPage";
-import {passGLOBAL} from "./pages/local-wallet-start/components/PasswordModal/PasswordModal";
+import PreviewPage from "./pages/PreviewPage/PreviewPage";
 
 
 const Router = () => {
 
     return (
         <div>
-            <passGLOBAL>
             <Routes>
                 <Route path='/' element={ <PreviewPage /> } />
-                <Route path='/choose' element={ <ChoosePage /> } />
-                <Route path='/lwstart' element={ <LocalWalletStartPage /> } />
-                <Route path='/transfer' element={ <TransfersPage /> } />
             </Routes>
-            </passGLOBAL>
         </div>
     );
 };
--- src/index.js
+++ src/index.js
@@ -24,8 +24,8 @@ if (require('electron-squirrel-startup')) {
 const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1200,
+    height: 680,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
     },
