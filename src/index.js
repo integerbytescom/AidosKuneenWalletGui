e8031c0a4db6f7d5ec3da514438ea8daf23d1711
integer <maxim.shnyagin@yandex.ru>
2022-08-03 13:19:14 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -215,7 +215,9 @@ const writeTxInHist = (tx) => {
 const send = async (evt, way, mempas, from, to, amount) => {
   console.log(`${prefix[plm]} send ${way} ${mempas} ${from} ${to} ${amount}`)
   try {
+    console.log("command start")
     const {stdout, stderr} = await exec(path.join(__dirname, `${prefix[plm]} send ${way} ${mempas} ${from} ${to} ${amount}`))
+    console.log("Result:"+stdout)
     const resp = JSON.parse(stdout)
     if (resp.data) {
       writeTxInHist(resp.data[0])
@@ -407,6 +409,7 @@ const stakedBalance = async (evt, ...addrs) => {
   }
 }
 
+const GAS = 0.021
 const GAS = 0.021
 const sendError = JSON.stringify({
   ok: false,
